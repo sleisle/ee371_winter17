4034e49b20f60e9aded8e4f0c9f304159f074292
sleisle <sleisle@gmail.com>
2017-01-17 08:31:02 -0800

    Fix everything

--- mux4_1.v
+++ mux4_1.v
@@ -1,16 +1,16 @@
 module mux4_1 (out, in, control);
 	input [3:0] in;
 	input [1:0] control;
-	output out;
+	output reg out;
 
 	// Case statement for mux output
-	always @(*)
+	always @(*) begin
 		case (control)
 			2'b00 : out = in[0];
 			2'b01 : out = in[1];
 			2'b10 : out = in[2];
 			2'b11 : out = in[3];
-			default : 
+			default : out = 1'bx;
 		endcase
 	end
 
