eae9bb0fb7f50671f4da10ec2b3857eb1f70ab33
sleisle <sleisle@gmail.com>
2017-01-19 08:31:16 -0800

    Add some to topLevel

--- lab2TopLevel.v
+++ lab2TopLevel.v
@@ -6,6 +6,14 @@ module lab2TopLevel (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW
 	output wire [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5; // Seven Segment Display
 	output wire [9:0] LEDR; // LEDs
 
-	
+	wire [13:0] outsideWaterLevel, lockWaterLevel, innerWaterLevel;
+
+	seg7 h0 ();
+	seg7 h1 ();
+	seg7 h2 ();
+	seg7 h3 ();
+	seg7 h4 ();
+	seg7 h5 ();
+	seg7 h6 ();
 
 endmodule
