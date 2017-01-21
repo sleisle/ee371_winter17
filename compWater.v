0acaa2c71ccb829370b1002aae3d01c9a29ac1a3
sleisle <sleisle@gmail.com>
2017-01-20 16:57:06 -0800

    Comp water

--- compWater.v
+++ compWater.v
@@ -1,10 +1,14 @@
 module compWater (rtn, water1, water2);
 	input wire [7:0] water1, water2;
-	output wire rtn;
-	parameter check1 = water1[7:4] * 10 + water1[3:0];
-	parameter check2 = water2[7:4] * 10 + water2[3:0];
-	parameter check = check2 - check1;
-	
-	assign rtn = (check > -3) | (check < 3);
+	output reg rtn;
+	reg [8:0] check1, check2, check;
+
+	always @(*) begin
+		check1 = water1;
+		check2 = water2;
+		check = (check2 - check1) + 3;
+		
+		rtn = (check > 0) & (check < 6);
+	end
 
 endmodule
