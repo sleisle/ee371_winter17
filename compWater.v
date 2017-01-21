a80f1e33c3593538e689f02890feeb29ce48a942
sleisle <sleisle@gmail.com>
2017-01-20 16:35:41 -0800

    Fix dumb Sean

--- compWater.v
+++ compWater.v
@@ -1,8 +1,8 @@
 module compWater (rtn, water1, water2);
-	input wire [5:0] water1, water2;
+	input wire [7:0] water1, water2;
 	output wire rtn;
-	parameter check1 = water1[5:3] * 10 + water1[2:0];
-	parameter check2 = water2[5:3] * 10 + water2[2:0];
+	parameter check1 = water1[7:4] * 10 + water1[3:0];
+	parameter check2 = water2[7:4] * 10 + water2[3:0];
 	parameter check = check2 - check1;
 	
 	assign rtn = (check > -3) | (check < 3);
