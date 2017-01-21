b08e102ac26d7635cb722c6f4b94985e8864a4e1
sleisle <sleisle@gmail.com>
2017-01-20 16:25:55 -0800

    Add more to lock system

--- lockSystem.v
+++ lockSystem.v
@@ -1,3 +1,13 @@
-module lockSystem ();
+module lockSystem (clk, rst, innerWater, lockWater, outerWater, outputs, inputs);
+	input wire clk, rst;
+	input wire [5:0] inputs; // Gondola arriving, Gondola departing, Outer Port, Inner Port, Increase Water, Decrease Water
+	output wire [3:0] outputs; // Gondola arriving, Gondola departing, Outer Port Status, Inner Port Status
+	output reg [5:0] innerWater, lockWater, outerWater;
+	
+	parameter outWaterStart = 7.3;
+	parameter inWaterStart = 4.9;
+	parameter lockWaterStart = 5.2;
+	
+	
 
 endmodule
