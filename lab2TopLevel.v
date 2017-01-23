a5edd1687aa8c7a030509298a98602c31f65c65d
sleisle <sleisle@gmail.com>
2017-01-22 20:27:22 -0800

    Fix stupid water thing

--- lab2TopLevel.v
+++ lab2TopLevel.v
@@ -22,6 +22,8 @@ module lab2TopLevel (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW
 	parameter whichClock = 23;
 	
 	// Initialize clock divider
+	assign LEDR[9] = clk;
+	assign LEDR[8] = ~KEY[0]; 
 	assign clk = clkMain[whichClock];
 	clock_divider cdiv (CLOCK_50, clkMain);
 
@@ -37,6 +39,6 @@ module lab2TopLevel (CLOCK_50, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW
 	seg7 h4 (outerWaterConv[3:0], HEX4);
 	seg7 h5 (outerWaterConv[7:4], HEX5);
 	
-	lockSystem lock (clk, ~KEY[0], innerWaterPre, lockWaterPre, outerWaterPre, {LEDR[0], LEDR[1], LEDR[2], LEDR[3]}, {SW[0], SW[1], SW[2], SW[3], ~KEY[1], ~KEY[2]});
+	lockSystem lock (clk, ~KEY[0], innerWaterPre, lockWaterPre, outerWaterPre, {LEDR[0], LEDR[1], LEDR[2], LEDR[3]}, {~KEY[2], ~KEY[1], SW[3], SW[2], SW[1], SW[0]});
 
 endmodule
