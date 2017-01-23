5a265527d98e71e55da503753637df231904fc0b
sleisle <sleisle@gmail.com>
2017-01-22 20:08:01 -0800

    Fix water stuff

--- lockSystem.v
+++ lockSystem.v
@@ -2,7 +2,8 @@ module lockSystem (clk, rst, innerWater, lockWater, outerWater, outputs, inputs)
 	input wire clk, rst;
 	input wire [5:0] inputs; // Gondola arriving, Gondola departing, Outer Port, Inner Port, Increase Water, Decrease Water
 	output wire [3:0] outputs; // Gondola arriving, Gondola departing, Outer Port Status, Inner Port Status
-	output reg [7:0] innerWater, lockWater, outerWater;
+	output wire [7:0] innerWater, outerWater;
+	output reg [7:0] lockWater;
 	
 	wire matchOuterWater, matchInnerWater;
 	reg outerPortStatus, innerPortStatus;
@@ -12,29 +13,29 @@ module lockSystem (clk, rst, innerWater, lockWater, outerWater, outputs, inputs)
 	assign outputs[2] = outerPortStatus; // 1 means closed
 	assign outputs[3] = innerPortStatus;
 	
-	always @(*) begin
-		outerWater = 8'd73;
-		innerWater = 8'd49;
-	end
+	assign outerWater = 8'd73;
+	assign innerWater = 8'd49;
 	
 	always @(posedge clk) begin
-		if (rst)
+		if (rst) begin
 			lockWater = 8'd52;
+			outerPortStatus = 1'b1;
+			innerPortStatus = 1'b1;
+		end
 		else begin
 			if (inputs[4] & outerPortStatus & innerPortStatus) begin // Increase Water
-				lockWater = lockWater + ((outerWater - innerWater) / 8'd16);
+				lockWater = lockWater + ((outerWater - innerWater) / 8'd12);
 			end
 			else if (inputs[5] & outerPortStatus & innerPortStatus) begin // Decrease Water
 				lockWater = lockWater - ((outerWater - innerWater) / 8'd14);
 			end
 			
-		end
-			
 		if (matchOuterWater)
 			outerPortStatus = inputs[2]; // Outer port = SW2
 		if (matchInnerWater)
 			innerPortStatus = inputs[3]; // Inner port = SW3
-	
+
+		end
 	end
 
 endmodule
