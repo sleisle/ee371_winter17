80678e399fca4582095579073d688c010678943a
Joseph Jimenez <jaj508@attu2.cs.washington.edu>
2017-01-26 16:51:16 -0800

    part 1/part2

--- Pointers/main.c
+++ Pointers/main.c
@@ -3,6 +3,8 @@
 
 int main()
 {
+    // Part 1
+    printf("Part 1\n");
     // Two variables of each type
     int int1 = 1;
     int int2 = 2;
@@ -23,9 +25,11 @@ int main()
     cp = &c2;
     printf("%d, %f, %c\n", *ip, *fp, *cp);
 
+    // Part 2
+    printf("Part 2\n");
     // Integers defined
     int A = 22;
-    int B = 22;
+    int B = 17;
     int C = 6;
     int D = 4;
     int E = 9;
